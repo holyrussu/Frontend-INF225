6602c021387b2b713ed34d7602ff2502dd2c6c83
Angelo <arussu00@gmail.com>
2024-04-18 16:20:28 -0400

    Primer commit

---
+++ components/Home.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+
+function Home() {
+  return (
+    <html>
+      <h1>Inicio</h1>
+      <Link to="/register">Registrar hora</Link><br/>
+      <Link to="/registerPaciente">Registrar paciente</Link><br/>
+      <Link to="/registerMedico">Registrar medico</Link><br/>
+      <Link to="/verHora">Buscar hora registrada</Link>
+    </html>
+  );
+}
+
+export default Home;
